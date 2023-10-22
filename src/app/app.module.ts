dcbb449cd3478462ae0404221f1f117ab1c920b0
20a91a0579@aec.edu.in <20a91a0579@aec.edu.in>
2023-10-22 14:25:46 +0530

    all

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { RepeatDirective } from './repeat.directive';
+@NgModule({
+  declarations: [
+    AppComponent,
+    RepeatDirective
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
